a4cf906d66340ad3eab31209cdecfe9f00542d59
Himanshu Rawat <himanshurawat90k@gmail.com>
2024-08-23 20:59:39 +0530

    configured get methods

--- server.js
+++ server.js
@@ -3,8 +3,36 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
-app.get('/jobs', (req, res) => {
-    res.json(data);
+let posts = [
+    {id: 1, title: 'post one'},
+    {id: 2, title: 'post two'},
+    {id: 3, title: 'post three'},
+];
+
+app.get('/api/posts', (req, res) => {
+
+    const limit = parseInt(req.query.limit);
+
+    if( !isNaN(limit) && limit > 0 ) {
+        return res.status(200).json(posts.slice(0, limit));
+    }
+    
+    res.status(200).json(posts);
+
+
+});
+
+app.get('/api/posts/:id', (req, res) => {
+    
+    const id = parseInt(req.params.id);
+    
+    const post = posts.find( (post) => post.id === id);
+
+    if(!post) {
+        return res.status(404).json( {msg: `The post with id ${id} was not found`} );
+    } 
+    
+    res.status(200).json(post);
 });
 
 app.listen(port, console.log(`server started at port ${port}`));
