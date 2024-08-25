b4767f8cd77bbfdc41b182a47088397bb470f6f9
Himanshu Rawat <himanshurawat90k@gmail.com>
2024-08-25 21:35:48 +0530

    Created Error Handler for all errors.

--- routes/posts.js
+++ routes/posts.js
@@ -7,17 +7,9 @@ let posts = [
     {id: 3, title: 'post three'},
 ];
 
-const logger = (req, res, next) => {
-    console.log(
-        `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
-    );
-
-    next();
-};
-
 // to get limited or all posts
 
-router.get('/', logger,  (req, res) => {
+router.get('/', (req, res, next) => {
 
     const limit = parseInt(req.query.limit);
 
@@ -32,28 +24,32 @@ router.get('/', logger,  (req, res) => {
 
 // to get single post
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
     
     const id = parseInt(req.params.id);
     
     const post = posts.find( (post) => post.id === id);
 
     if(!post) {
-        return res.status(404).json( {msg: `The post with id ${id} was not found`} );
+        const error = new Error(`A post with the id of ${id} was not found`);
+        error.status = 404;
+        return next(error);
     } 
     
     res.status(200).json(post);
 });
 
 // Create new post
-router.post('/', ( req, res ) => {
+router.post('/', ( req, res, next ) => {
     const newPost = {
         id: posts.length +1,
         title: req.body.title,
     };
 
     if(!newPost.title) {
-        return res.status(400).json({msg: "please add the title"});
+        const error = new Error(`Please include the title`);
+        error.status = 400;
+        return next(error);
     }
     posts.push(newPost);
     res.status(201).json(posts);
@@ -61,24 +57,28 @@ router.post('/', ( req, res ) => {
 
 
 // Update post
-router.put('/:id', ( req, res ) => {
+router.put('/:id', ( req, res, next ) => {
     const id = parseInt(req.params.id);
     const post = posts.find((post) => post.id === id);
 
     if(!post) {
-        return res.status(404).json(`The post with id ${id} was not found`);
+        const error = new Error(`The post with id ${id} was not found`);
+        error.status = 404;
+        return next(error);
     }
     post.title = req.body.title;
     res.status(200).json(posts);
 });
 
 // Delete post
-router.delete('/:id', ( req, res ) => {
+router.delete('/:id', ( req, res, next ) => {
     const id = parseInt(req.params.id);
     const post = posts.find((post) => post.id === id);
 
     if(!post) {
-        return res.status(404).json(`The post with id ${id} was not found`);
+        const error = new Error(`The post with id ${id} was not found`);
+        error.status = 404;
+        return next(error);
     }
     posts = posts.filter((post) => post.id !== id);
     res.status(200).json(posts);
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 const port = process.env.PORT || 8000;
+import logger from './middleware/logger.js';
 import posts from './routes/posts.js';
+import errorHandler from './middleware/error.js';
+import notFound from './middleware/notFound.js';
 
 const app = express();
 
@@ -8,7 +11,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded( {extended: false} ));
 
+// Middleware
+app.use(logger);
+
 // Routes
 app.use('/api/posts', posts);
+
+app.use(notFound);
+
+// ErrorHandler
+app.use(errorHandler);
+
 
 app.listen(port, console.log(`server started at port ${port}`));
